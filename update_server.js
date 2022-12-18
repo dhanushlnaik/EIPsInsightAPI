1de6e9c7dccbc9e40bae799d37f3a78f49effd39
KundlasRitik <ritikkundlas@gmail.com>
2022-12-18 19:58:36 +0530

    updated current month api

--- update_server.js
+++ update_server.js
@@ -13,23 +13,27 @@ const dirPath = path.join(__dirname,'EIPS');
 const {Eip_model ,status_schema} = require('./model/eipSchema');
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/eipsdata" ,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-const conn = mongoose.createConnection("mongodb://127.0.0.1:27017/eips", {
+mongoose.connect("mongodb://localhost:27017/eipsdata" ,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, (err)=>{
     if(err)
     console.log(err);
     else
-    console.log("eips DB connected");
+    console.log("eips monthly connected");}
+    );
+
+const conn = mongoose.createConnection("mongodb://localhost:27017/eips", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err)=>{
+    if(err)
+    console.log(err);
+    else
+    console.log("eips all data connected");
 });
 
 const StatusModel = conn.model('statuseipinfo', status_schema);
-
 
 
 const app = express();
@@ -39,13 +43,36 @@ const dir = dirPath;
 
 const d = new Date();
 let stringDate = d.toString();
-
-let cur_day = stringDate.substring(8,10);
+let full_curMonth;
 let cur_month = stringDate.substring(4,7);
+
+if(cur_month == "Jan")
+full_curMonth = "January";
+else if(cur_month == "Feb")
+full_curMonth = "February";
+else if(cur_month == "Mar")
+full_curMonth = "March";
+else if(cur_month == "Apr")
+full_curMonth = "April";
+else if(cur_month == "Jun")
+full_curMonth = "June";
+else if(cur_month == "Jul")
+full_curMonth = "July";
+else if(cur_month == "Aug")
+full_curMonth = "August";
+else if(cur_month == "Sep")
+full_curMonth = "September";
+else if(cur_month == "Oct")
+full_curMonth = "October";
+else if(cur_month == "Nov")
+full_curMonth = "November";
+else if(cur_month == "Dec")
+full_curMonth = "December";
+
 let cur_year = stringDate.substring(11,15);
 
 let statusEIP = ['Final','Draft','Review','Last_Call','Stagnant','Withdrawn','Living' ];
-let categoryEIP = ['Networking','ERC','Core','Interface','Meta', 'Informational'];
+let categoryEIP = ['Networking','ERC','Core','Interface','Meta', 'Informational', 'Undefined'];
 let new_files = {};
 let cur_month_data = {};
 
@@ -76,11 +103,7 @@ function update_eips(eip , status ,category , type)
         console.log(err);
         else
         {
-            console.log("updated-eips-databse");
-            // console.log("changing from " + match.status + "to " + status);
-            // cur_month_data[eip] = {"status" : status , "category" : category , "type" : type};
-            // console.log(cur_month_data);
-           
+            console.log("updated-eips-databse");         
         }
 
     });
@@ -93,10 +116,13 @@ function compare2(eip , type ,category , status)
         // console.log(result + "here");
         
           if(err)
-          console.log("NOT FOUND");
+          console.log(err);
           else if(result.length == 0)
           {
             // console.log(result);
+
+            //   if(category == undefined )
+            //   category = 'Undefined';
 
               Eip_model.create({"eip": eip , "type" : type , "category": category, "status" : status}, (err)=>{
                 
@@ -119,7 +145,7 @@ function compare2(eip , type ,category , status)
                         ar1.push(eip);
                         ar2.push(eip);
                         cur_month_data[status][category] = ar1;
-                        cur_month_data[status][type] = ar2
+                        cur_month_data[status][type] = ar2;
                         // cur_month_data[status][category].push(eip);
                         // cur_month_data[status][type].push(eip);
                     }
@@ -143,7 +169,10 @@ function compare2(eip , type ,category , status)
             
                if(match.status != status || match.type != type || match.category != category)
                {
-                 // Update mongoDB monthly data}
+                 // Update mongoDB monthly data
+
+                //  if(category == undefined)
+                //  category = 'Undefined';
 
                  if(type != "Meta" && type != "Informational")
                     {
@@ -165,9 +194,6 @@ function compare2(eip , type ,category , status)
 
                     cur_month_data[status].change = "true";
 
-                    // console.log(cur_month_data);
-               
-                //   update_monthly(eip , status , category , type);
 
                 update_eips(eip , status, category ,type);
 
@@ -180,65 +206,176 @@ function compare2(eip , type ,category , status)
 }
 
 function readFiles(){
-fs.readdir(dir, function(err, files){
-
-    if(err)
-    {
-        console.log("Encountered error");
-    }
-
-    files.forEach(function(file){
-        
-        let newPath = dir + '/' + file;
-        let { mtime }  = fs.statSync(newPath);
-        
-        let stdate = mtime.toString();
-        
-        let day = stdate.substring(8,10);
-        let month = stdate.substring(4,7);
-        let year = stdate.substring(11,15);
-
-        if(year  == cur_year && month == cur_month)
+   
+    fs.readdir(dir, function(err, files){
+    
+        if(err)
         {
-            fs.readFile( `${dir}/${file}` ,'utf8', function(err, content ){
+            console.log("Encountered error");
+        }
+    
+        files.forEach(function(file){
             
-                let eipfile = matter(content);
-                let data = eipfile.data;
-                let eip = data.eip;
-                let type = eipfile.data.type;
-                let status = eipfile.data.status;
-                let category = eipfile.data.category;
+            let newPath = dir + '/' + file;
+            let { mtime }  = fs.statSync(newPath);
+            
+            let stdate = mtime.toString();
+            
+            let day = stdate.substring(8,10);
+            let month = stdate.substring(4,7);
+            let year = stdate.substring(11,15);
+    
+            if(year  == cur_year && month == cur_month)
+            {
+                fs.readFile( `${dir}/${file}` ,'utf8', function(err, content ){
+                
+                    let eipfile = matter(content);
+                    let data = eipfile.data;
+                    let eip = data.eip;
+                    let type = eipfile.data.type;
+                    let status = eipfile.data.status;
+                    let category = eipfile.data.category;
 
-                if(status == "Last Call")
-                status = "Last_Call";
-                if(type == "Standards Track")
-                type = "Standard_Track";
+                    if(category == undefined)
+                    category = 'Undefined';
+    
+                    if(status == "Last Call")
+                    status = "Last_Call";
+                    if(type == "Standards Track")
+                    type = "Standard_Track";
+    
+                    let eip_string = eip.toString();
+    
+                    new_files["eip-"+eip_string] = {type : type , category : category , status : status};
+                    console.log(new_files);
+                    // setTimeout(compare , 1000);
+                    // compare( "eip-"+eip_string, type ,category , status);
+    
+                    compare2("eip-"+eip_string, type ,category , status);
+                   
+                });
+      
+            }
+        });
+    
+        // console.log(new_files);
+         
+    })
+   
+}
 
-                let eip_string = eip.toString();
-
-                new_files["eip-"+eip_string] = {type : type , category : category , status : status};
-                console.log(new_files);
-                // setTimeout(compare , 1000);
-                // compare( "eip-"+eip_string, type ,category , status);
-
-                compare2("eip-"+eip_string, type ,category , status);
-               
-            });
-  
+function update_doc( status , arr_net , arr_core , arr_erc , arr_info , arr_inter , arr_meta , arr_undef)
+{
+    StatusModel.updateOne({"Status" : status , "Month" : full_curMonth , "Year" : cur_year}, {"Networking": arr_net , "ERC": arr_erc ,"Core": arr_core ,"Interface": arr_inter ,"Meta": arr_meta, "Informational": arr_info , "Undefined" : arr_undef },{new: true}, (err , docs)=>{
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log("updated current month data"); 
         }
     });
-
-    // console.log(new_files);
-     
-})
-
 }
+
+function createNewRecord(status, data_add)
+{
+
+    StatusModel.create({"Year" : cur_year , "Month" : full_curMonth , "Status" : status , "Networking" : [0] , "ERC": [0] ,"Core": [0] ,"Interface": [0] ,"Meta": [0], "Informational": [0] , "Undefined" : [0]}, (err) =>{
+        if(err)
+        console.log(err);
+        else
+        {
+            updateRecord(status , data_add);
+        }
+      });
+         
+}
+
+function updateRecord(status , data_add)
+{
+    StatusModel.find({"Status" : status ,"Year" : cur_year , "Month" : full_curMonth} ,(err , res) =>{
+        if(err)
+        console.log(err);
+        else
+        {
+            let pre =  res[0];
+            console.log(res);
+            console.log(cur_year +" "+ full_curMonth +" "+status );
+
+            let cnt_net  =  data_add[status].Networking;
+            let cnt_erc  =  data_add[status].ERC ;
+            let cnt_core =  data_add[status].Core;
+            let cnt_inter = data_add[status].Interface;
+            let cnt_meta =  data_add[status].Meta; 
+            let cnt_info =  data_add[status].Informational;
+            let cnt_undef =  data_add[status].Undefined;
+
+            let arr_net = pre.Networking , arr_erc = pre.ERC , arr_core = pre.Core , arr_inter = pre.Interface , arr_meta  = pre.Meta , arr_info = pre.Informational , arr_undef = pre.Undefined ;
+            arr_net[0] = cnt_net.toString();
+            arr_erc[0] = cnt_erc.toString();
+            arr_core[0] = cnt_core.toString();
+            arr_inter[0] = cnt_inter.toString();
+            arr_meta[0] = cnt_meta.toString();
+            arr_info[0] = cnt_info.toString();
+            arr_undef[0] = cnt_undef.toString();
+
+            let names_net = cur_month_data[status].Networking;
+
+            names_net.forEach((data)=>{
+            arr_net.push(data);
+           })
+
+           let names_erc = cur_month_data[status].ERC;
+
+            names_erc.forEach((data)=>{
+            arr_erc.push(data);
+           })
+
+           let names_core = cur_month_data[status].Core;
+
+            names_core.forEach((data)=>{
+            arr_core.push(data);
+           })
+
+           let names_inter = cur_month_data[status].Interface;
+
+            names_inter.forEach((data)=>{
+            arr_inter.push(data);
+           })
+
+           let names_meta = cur_month_data[status].Meta;
+
+            names_meta.forEach((data)=>{
+            arr_meta.push(data);
+           })
+
+           let names_info = cur_month_data[status].Informational;
+
+            names_info.forEach((data)=>{
+            arr_info.push(data);
+           })
+
+           let names_undef = cur_month_data[status].Undefined;
+
+            names_undef.forEach((data)=>{
+            arr_undef.push(data);
+           })
+
+
+           update_doc(status , arr_net , arr_core , arr_erc , arr_info , arr_inter , arr_meta , arr_undef );
+            
+        }
+    })
+}
+
 
 setTimeout(afterWait , 10000);
 
-
-
-
+function waitToCreate()
+{
+    // Do nothing , waste some timem here
+}
 
 function afterWait()
 {
@@ -266,113 +403,95 @@ function afterWait()
 
     console.log(data_add);
 
-
-   
-
     for(let status in data_add)
     {
         if(cur_month_data[status].change == "true")
         {
-            StatusModel.find({"Status" : status}, (err ,res)=>{
+            StatusModel.find({"Status" : status , "Month" : full_curMonth , "Year" : cur_year}, (err ,res)=>{
                 if(err)
                 console.log(err);
                 else
                 {
-                  let pre = res[0];
-
-                   let cnt_net  =  Number(pre.Networking[0]) + data_add[status].Networking;
-                   let cnt_erc  =  Number(pre.ERC[0]) + data_add[status].ERC ;
-                   let cnt_core =  Number(pre.Core[0]) + data_add[status].Core;
-                   let cnt_inter = Number(pre.Interface[0]) + data_add[status].Interface;
-                   let cnt_meta =  Number(pre.Meta[0]) + data_add[status].Meta; 
-                   let cnt_info =  Number(pre.Informational[0]) + data_add[status].Informational;
-
-
-
-                   let arr_net = pre.Networking , arr_erc = pre.ERC , arr_core = pre.Core , arr_inter = pre.Interface , arr_meta  = pre.Meta , arr_info = pre.Informational;
-                   arr_net[0] = cnt_net.toString();
-                   arr_erc[0] = cnt_erc.toString();
-                   arr_core[0] = cnt_core.toString();
-                   arr_inter[0] = cnt_inter.toString();
-                   arr_meta[0] = cnt_meta.toString();
-                   arr_info[0] = cnt_info.toString();
-
-                   
-                   let names_net = cur_month_data[status].Networking;
-
-                    names_net.forEach((data)=>{
-                    arr_net.push(data);
-                   })
-
-                   let names_erc = cur_month_data[status].ERC;
-
-                    names_erc.forEach((data)=>{
-                    arr_erc.push(data);
-                   })
-
-                   let names_core = cur_month_data[status].Core;
-
-                    names_core.forEach((data)=>{
-                    arr_core.push(data);
-                   })
-
-                   let names_inter = cur_month_data[status].Interface;
-
-                    names_inter.forEach((data)=>{
-                    arr_inter.push(data);
-                   })
-
-                   let names_meta = cur_month_data[status].Meta;
-
-                    names_meta.forEach((data)=>{
-                    arr_meta.push(data);
-                   })
-
-                   let names_info = cur_month_data[status].Informational;
-
-                    names_info.forEach((data)=>{
-                    arr_info.push(data);
-                   })
-
-                   StatusModel.updateOne({"Status" : status}, {"Networking": arr_net , "ERC": arr_erc ,"Core": arr_core ,"Interface": arr_inter ,"Meta": arr_meta, "Informational": arr_info },{new: true}, (err , docs)=>{
-                    if(err)
+                    if(res.length == 0)
                     {
-                        console.log(err);
+                        createNewRecord(status, data_add);
+                        setTimeout( waitToCreate , 30000);
                     }
                     else
                     {
-                        console.log("updated current month data"); 
+                        let pre = res[0];
+
+                        let cnt_net  =  Number(pre.Networking[0]) + data_add[status].Networking;
+                        let cnt_erc  =  Number(pre.ERC[0]) + data_add[status].ERC ;
+                        let cnt_core =  Number(pre.Core[0]) + data_add[status].Core;
+                        let cnt_inter = Number(pre.Interface[0]) + data_add[status].Interface;
+                        let cnt_meta =  Number(pre.Meta[0]) + data_add[status].Meta; 
+                        let cnt_info =  Number(pre.Informational[0]) + data_add[status].Informational;
+                        let cnt_undef =  Number(pre.Undefined[0]) + data_add[status].Undefined;
+     
+     
+     
+                        let arr_net = pre.Networking , arr_erc = pre.ERC , arr_core = pre.Core , arr_inter = pre.Interface , arr_meta  = pre.Meta , arr_info = pre.Informational, arr_undef = pre.Undefined;
+                        arr_net[0] = cnt_net.toString();
+                        arr_erc[0] = cnt_erc.toString();
+                        arr_core[0] = cnt_core.toString();
+                        arr_inter[0] = cnt_inter.toString();
+                        arr_meta[0] = cnt_meta.toString();
+                        arr_info[0] = cnt_info.toString();
+                        arr_undef[0] = cnt_undef.toString();
+     
+                        
+                        let names_net = cur_month_data[status].Networking;
+     
+                         names_net.forEach((data)=>{
+                         arr_net.push(data);
+                        })
+     
+                        let names_erc = cur_month_data[status].ERC;
+     
+                         names_erc.forEach((data)=>{
+                         arr_erc.push(data);
+                        })
+     
+                        let names_core = cur_month_data[status].Core;
+     
+                         names_core.forEach((data)=>{
+                         arr_core.push(data);
+                        })
+     
+                        let names_inter = cur_month_data[status].Interface;
+     
+                         names_inter.forEach((data)=>{
+                         arr_inter.push(data);
+                        })
+     
+                        let names_meta = cur_month_data[status].Meta;
+     
+                         names_meta.forEach((data)=>{
+                         arr_meta.push(data);
+                        })
+     
+                        let names_info = cur_month_data[status].Informational;
+     
+                         names_info.forEach((data)=>{
+                         arr_info.push(data);
+                        })
+
+                        let names_undef = cur_month_data[status].Undefined;
+
+                        names_undef.forEach((data)=>{
+                        arr_undef.push(data);
+                       })
+     
+                        update_doc(status , arr_net , arr_core , arr_erc , arr_info , arr_inter , arr_meta , arr_undef );
                     }
-                });
                 }
             });   
         }
     }
 }
 
-// function getData()
-// {
-//     StatusModel.find({} , (err,res)=>{
-//         if(err)
-//         console.log(err);
-//         else
-//         {
-//             let monthlyUpdate = (res);
-//             console.log(monthlyUpdate);          
-             
-//             return monthlyUpdate;
-//             // module.exports = monthlyUpdate;
-//         }
-//     })
-// }
 
-
-
-// function send(){
-
-
-
-// }
 
 
 
